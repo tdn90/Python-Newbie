3be2fcd8140a4211d9ee7f5cc6635b70b1289303
tdn90 <nguyentridung1212@gmail.com>
2018-10-17 00:54:43 -0400

    Complete implementing an analyzer for text documents

---
+++ TextDocAnalyzer/ResultGenerator.py
@@ -0,0 +1,15 @@
+from scanFile import *
+
+def getResult():
+    wordDict = count()
+    words = list(wordDict.keys())
+    words.sort()
+    file = open("output.txt", 'w')
+    numWordsRead = 0
+    for word in words:
+        file.write('\t{0:>5}\t{1:<}\n'.format(wordDict.get(word), word))
+        numWordsRead += wordDict.get(word)
+    file.write("-----------------------\n")
+    file.write('\t{0:>5}\t{1:<}\n'.format(len(words), 'Number of distinct words'))
+    file.write('\t{0:>5}\t{1:<}\n'.format(numWordsRead, 'Total words read'))
+    file.close()
